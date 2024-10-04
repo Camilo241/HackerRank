d2725f96bcbbd99c9b79c8b8a44c495d0bde0397
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-10-04 09:41:28 -0500

    :construction:

--- Java/Java/Solution.java
+++ Java/Java/Solution.java
@@ -18,18 +18,18 @@ public class Solution {
 
     public static void main(String[] args) {
        try{
-			BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-			int n1=Integer.parseInt(br.readLine());
-			int n2=Integer.parseInt(br.readLine());
-			int n3=Integer.parseInt(br.readLine());
-			int n4=Integer.parseInt(br.readLine());
-			int n5=Integer.parseInt(br.readLine());
-			int n6=Integer.parseInt(br.readLine());
-			Add ob=new Add();
-			ob.add(n1,n2);
-			ob.add(n1,n2,n3);
-			ob.add(n1,n2,n3,n4,n5);	
-			ob.add(n1,n2,n3,n4,n5,n6);
+			// BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+			// int n1=Integer.parseInt(br.readLine());
+			// int n2=Integer.parseInt(br.readLine());
+			// int n3=Integer.parseInt(br.readLine());
+			// int n4=Integer.parseInt(br.readLine());
+			// int n5=Integer.parseInt(br.readLine());
+			// int n6=Integer.parseInt(br.readLine());
+			// Add ob=new Add();
+			// ob.add(n1,n2);
+			// ob.add(n1,n2,n3);
+			// ob.add(n1,n2,n3,n4,n5);	
+			// ob.add(n1,n2,n3,n4,n5,n6);
 			Method[] methods=Add.class.getDeclaredMethods();
 			Set<String> set=new HashSet<>();
 			boolean overload=false;
