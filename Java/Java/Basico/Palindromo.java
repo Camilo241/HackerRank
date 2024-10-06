cee227c81ddf36d03683f7849c1f7abbdd58e372
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-10-06 17:44:11 -0500

    que bobada de error :construction:

--- Java/Java/Basico/Palindromo.java
+++ Java/Java/Basico/Palindromo.java
@@ -1,5 +1,25 @@
 package Java.Basico;
 
+import java.util.Scanner;
+
 public class Palindromo {
-    
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B = "";
+        sc.close();
+        for (int i = 0 ; i < A.length(); i++) {
+            B = A.charAt(i) + B; 
+            System.out.println(B);
+        }
+        if (A.equals(B)) {
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+        
+        
+        
+    }
 }
