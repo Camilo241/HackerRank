2fdd5ba25db4136c003cea6054ea84d26141df98
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-10-06 13:06:51 -0500

    Facilito :construction:

---
+++ Java/Java/Medio/Metodo_Sobrecargado.java
@@ -0,0 +1,36 @@
+package Java.Medio;
+
+class Sports{
+
+    String getName(){
+        return "Generic Sports";
+    }
+
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+class Soccer extends Sports{
+    @Override // se utiliza para cambiar el metodo
+    String getName(){
+        return "Soccer Class";
+    }
+
+    void getNumberOfTeamMembers(){
+        System.out.println( "Each team has n players in " + getName() );
+    }
+}
+
+
+
+public class Metodo_Sobrecargado {
+    public static void main(String []args){
+        Sports c1 = new Sports();
+        Soccer c2 = new Soccer();
+        System.out.println(c1.getName());
+        c1.getNumberOfTeamMembers();
+        System.out.println(c2.getName());
+        c2.getNumberOfTeamMembers();
+	}
+}
