ce1040d1cf8599f9ab4907519ceab9cf74fbb068
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-10-06 14:49:53 -0500

    este si no entendi ni monda jajja :construction:

---
+++ Java/Java/Medio/Patron_Singleton.java
@@ -0,0 +1,22 @@
+package Java.Medio;
+
+
+    class Singleton{
+        private static Singleton instance;
+        public String str;
+    
+    private Singleton() {
+    }
+    
+    public static Singleton getSingleInstance() {
+        if(instance == null) {
+            synchronized(Singleton.class) {
+                if(instance == null) {
+                    instance = new Singleton();
+                }
+            }
+        }
+        return instance;
+    }
+    }
+    
