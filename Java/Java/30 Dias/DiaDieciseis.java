64a4502ff3aa2d25c428bdefefbf6e836dc44a1c
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-11-21 15:14:55 -0500

    Terminamos dia16 exepciones :construction:

---
+++ Java/Java/30 Dias/DiaDieciseis.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class DiaDieciseis {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = bufferedReader.readLine();
+
+        try {
+            int n = Integer.parseInt(S);
+            System.out.println(n);
+        } catch (NumberFormatException e) {
+            System.out.println("Bad String");
+        }
+
+        bufferedReader.close();
+    }
+}
