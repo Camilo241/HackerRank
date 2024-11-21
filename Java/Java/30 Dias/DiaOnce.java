42b852b9b68a0f28f13789c1562b15ee02505f70
Camilo241 <cristiancamilofandinomendieta@gmail.com>
2024-11-21 16:14:37 -0500

    Practicar Matrices de dos dimensiones :construction:

--- Java/Java/30 Dias/DiaOnce.java
+++ Java/Java/30 Dias/DiaOnce.java
@@ -1,26 +1,33 @@
 import java.io.*;
 import java.util.*;
-import java.util.stream.*;
-import static java.util.stream.Collectors.toList;
+
 
 public class DiaOnce {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
         List<List<Integer>> arr = new ArrayList<>();
+        for (int i = 0; i < 6; i++) {
+            String[] arrRowTempItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-        IntStream.range(0, 6).forEach(i -> {
-            try {
-                arr.add(
-                    Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-                        .map(Integer::parseInt)
-                        .collect(toList())
-                );
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
+            List<Integer> arrRowItems = new ArrayList<>();
+
+            for (int j = 0; j < 6; j++) {
+                int arrItem = Integer.parseInt(arrRowTempItems[j]);
+                arrRowItems.add(arrItem);
             }
-        });
 
+            arr.add(arrRowItems);
+        }
+        int m, max = Integer.MIN_VALUE;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                m = arr.get(i).get(j) + arr.get(i).get(j + 1) + arr.get(i).get(j + 2) + arr.get(i + 1).get(j + 1)
+                        + arr.get(i + 2).get(j) + arr.get(i + 2).get(j + 1) + arr.get(i + 2).get(j + 2);
+                max = Math.max(m, max);
+            }
+        }
+        System.out.println(max);
         bufferedReader.close();
     }
 
